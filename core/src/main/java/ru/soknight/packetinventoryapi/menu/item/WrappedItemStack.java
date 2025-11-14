2f75683e40e49d53309d3d5d683693a1aee5a551
Mrflaxe <dmitriyclon@gmail.com>
2025-11-14 15:39:08 +0400

    [Fix]: StackOverFlow exception in WrappedItemStack with paper 1.21.8 core

--- core/src/main/java/ru/soknight/packetinventoryapi/menu/item/WrappedItemStack.java
+++ core/src/main/java/ru/soknight/packetinventoryapi/menu/item/WrappedItemStack.java
@@ -10,21 +10,23 @@ import ru.soknight.packetinventoryapi.nms.vanilla.VanillaItem;
 public final class WrappedItemStack extends ItemStack {
 
     private final VanillaItem<?, ?> vanillaItem;
+    private final ItemStack delegate;
 
     public WrappedItemStack(@NotNull Material type, @NotNull VanillaItem<?, ?> vanillaItem) {
         super(type);
         this.vanillaItem = vanillaItem;
+        this.delegate = super.clone();
     }
 
     public WrappedItemStack(@NotNull ItemStack itemStack, @NotNull VanillaItem<?, ?> vanillaItem) {
-        super(itemStack);
+        super(itemStack.clone());
         this.vanillaItem = vanillaItem;
+        this.delegate = itemStack.clone();
     }
 
     @Override
     @SuppressWarnings("MethodDoesntCallSuperMethod")
     public @NotNull WrappedItemStack clone() {
-        return new WrappedItemStack(this, vanillaItem);
+        return new WrappedItemStack(delegate.clone(), vanillaItem);
     }
-
 }
